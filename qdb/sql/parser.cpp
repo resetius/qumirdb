c26bf31d345be207fae990e43da01608f33b770a
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-06-21 00:54:36 +0100

    Continue to work on sql parser

--- qdb/sql/parser.cpp
+++ qdb/sql/parser.cpp
@@ -38,7 +38,7 @@ bool IsOp(const TToken& token, char op) {
     return token.Type == TToken::Operator && token.Value.i64 == static_cast<int64_t>(op);
 }
 
-bool isKeyword(const TToken& tok, const std::string& kw) {
+bool IsKeyword(const TToken& tok, const std::string& kw) {
     return tok.Type == TToken::Keyword && tok.Name == kw;
 }
 
@@ -46,10 +46,10 @@ template<class F>
 using TTask = std::invoke_result_t<F&, TParserContext&>;
 
 template<typename T>
-TTask<T> tryKeywords(T&& lambda, TParserContext& ctx, const std::vector<std::string>& kws) {
+TTask<T> TryKeywords(T&& lambda, TParserContext& ctx, const std::vector<std::string>& kws) {
     for (const auto& kw : kws) {
         auto token = ctx.Stream.Next();
-        if (!isKeyword(token, kw)) {
+        if (!IsKeyword(token, kw)) {
             co_return {};
         }
     }
@@ -61,6 +61,7 @@ TAstTask<TSqlQuery> select_stmt(TParserContext& ctx);
 TAstTask<TSqlNode> select_core(TParserContext& ctx);
 TAstTask<TSqlOrder> order_by_clause(TParserContext& ctx);
 TAstTask<TSqlWithClause> with_clause(TParserContext& ctx);
+TAstTask<TSqlCte> cte_def(TParserContext& ctx);
 TAstExprTask limit_clause(TParserContext& ctx);
 TAstExprTask offset_clause(TParserContext& ctx);
 
@@ -81,19 +82,32 @@ TAstTask<TSqlQuery> select_stmt(TParserContext& ctx) {
     TSqlPtr<TSqlQuery> q = std::make_shared<TSqlQuery>();
     auto token = ctx.Stream.Next();
 
-    q->WithClause = co_await tryKeywords(with_clause, ctx, {"WITH"});
+    q->WithClause = co_await TryKeywords(with_clause, ctx, {"WITH"});
 
     q->Body = co_await select_core(ctx);
 
-    q->OrderBy = co_await tryKeywords(order_by_clause, ctx, {"ORDER", "BY"});
-    q->Limit = co_await tryKeywords(limit_clause, ctx, {"LIMIT"});
-    q->Offset = co_await tryKeywords(offset_clause, ctx, {"OFFSET"});
+    q->OrderBy = co_await TryKeywords(order_by_clause, ctx, {"ORDER", "BY"});
+    q->Limit = co_await TryKeywords(limit_clause, ctx, {"LIMIT"});
+    q->Offset = co_await TryKeywords(offset_clause, ctx, {"OFFSET"});
 
     co_return q;
 }
 
 TAstTask<TSqlWithClause> with_clause(TParserContext& ctx) {
-    TSqlPtr<TSqlWithClause> with;
+    auto with = std::make_shared<TSqlWithClause>();
+    auto token = ctx.Stream.Next();
+    if (IsKeyword(token, "RECURSIVE")) {
+        with->Recursive = true;
+    } else {
+        ctx.Stream.Unget(token);
+    }
+
+    do {
+        auto cte = co_await cte_def(ctx);
+        token = ctx.Stream.Next();
+    } while (IsOp(token, ','));
+    ctx.Stream.Unget(token);
+
     co_return with;
 }
 
@@ -110,6 +124,10 @@ TAstExprTask limit_clause(TParserContext& ctx) {
 }
 
 TAstExprTask offset_clause(TParserContext& ctx) {
+    co_return nullptr;
+}
+
+TAstTask<TSqlCte> cte_def(TParserContext& ctx) {
     co_return nullptr;
 }
 
