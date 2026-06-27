c25ab555a573d07ef970a6c8111d17f835ad2bfc
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-06-27 09:22:55 +0100

    Add sort cases

--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -23,13 +23,33 @@ sort by 0
 
 */
 
-void CountSortIndices(uint32_t* dest, uint32_t* indices, uint32_t* work, int n, int digit)
+template<typename T>
+std::make_unsigned_t<T> RadixKey(T x) {
+    using U = std::make_unsigned_t<T>;
+    U u = std::bit_cast<U>(x);
+
+    if constexpr (std::is_signed_v<T>) {
+        u ^= (U(1) << (sizeof(T) * 8 - 1));
+    }
+
+    return u;
+}
+
+uint64_t RadixKey(double x) {
+    uint64_t u = std::bit_cast<uint64_t>(x);
+    uint64_t sign = u >> 63;
+    uint64_t mask = (-sign) | 0x8000000000000000ULL;
+    return u ^ mask;
+}
+
+template<typename T>
+void CountSortIndices(T* dest, uint32_t* indices, T* work, int n, int digit)
 {
     // base-16, 8 bits
     uint32_t counts[256] = {0};
     // count digits
     for (int i = 0; i < n; i++) {
-        counts[(dest[indices[i]] >> digit) & 0xffU]++;
+        counts[(RadixKey(dest[indices[i]]) >> digit) & 0xffU]++;
     }
     // counts -> places
     for (int i = 1; i < 256; i++) {
@@ -37,7 +57,7 @@ void CountSortIndices(uint32_t* dest, uint32_t* indices, uint32_t* work, int n, 
     }
 
     for (int i = n-1; i >= 0; --i) {
-        auto index = (dest[indices[i]] >> digit) & 0xffU;
+        auto index = (RadixKey(dest[indices[i]]) >> digit) & 0xffU;
         auto place = counts[index]-1;
         work[place] = indices[i];
         counts[index]--;
@@ -48,8 +68,9 @@ void CountSortIndices(uint32_t* dest, uint32_t* indices, uint32_t* work, int n, 
     }
 }
 
-void RadixSortIndices(uint32_t* dest, uint32_t* indices, uint32_t* work, int n) {
-    for (int i = 0; i < sizeof(uint32_t) * 8; i += 8) {
+template<typename T>
+void RadixSortIndices(T* dest, uint32_t* indices, T* work, int n) {
+    for (int i = 0; i < sizeof(T) * 8; i += 8) {
         CountSortIndices(dest, indices, work, n, i);
     }
 }
@@ -111,6 +132,30 @@ TEST(SortTest, Basic) {
 TEST(SortTest, BasicIndices) {
     std::vector<uint32_t> ar = {2345, 123498, 123, 1, 2, 555, 10};
     std::vector<uint32_t> work; work.resize(ar.size());
+    std::vector<uint32_t> indices; indices.resize(ar.size());
+    std::iota(indices.begin(), indices.end(), 0);
+    RadixSortIndices(ar.data(), indices.data(), work.data(), ar.size());
+    for (int i = 0; i < ar.size(); i++) {
+        std::cerr << ar[indices[i]] << ", ";
+    }
+    std::cerr << "\n";
+}
+
+TEST(SortTest, BasicIndicesSigned) {
+    std::vector<int32_t> ar = {-2345, 123498, -123, 1, -2, 555, 10};
+    std::vector<int32_t> work; work.resize(ar.size());
+    std::vector<uint32_t> indices; indices.resize(ar.size());
+    std::iota(indices.begin(), indices.end(), 0);
+    RadixSortIndices(ar.data(), indices.data(), work.data(), ar.size());
+    for (int i = 0; i < ar.size(); i++) {
+        std::cerr << ar[indices[i]] << ", ";
+    }
+    std::cerr << "\n";
+}
+
+TEST(SortTest, BasicIndicesDouble) {
+    std::vector<double> ar = {-2345.6, 123498.1, -123.5, 1.2, -2.5, 555.99, 10.12344, std::numeric_limits<double>::quiet_NaN()};
+    std::vector<double> work; work.resize(ar.size());
     std::vector<uint32_t> indices; indices.resize(ar.size());
     std::iota(indices.begin(), indices.end(), 0);
     RadixSortIndices(ar.data(), indices.data(), work.data(), ar.size());
