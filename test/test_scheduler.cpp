64b36d5a9a040ded876d521bca209d96e37c962d
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-06-29 00:46:08 +0100

    Experiments

--- test/test_scheduler.cpp
+++ test/test_scheduler.cpp
@@ -3,6 +3,7 @@
 #include <deque>
 #include <vector>
 #include <list>
+#include <unordered_set>
 
 namespace {
 
@@ -45,127 +46,194 @@ enum class EState {
     FINISHED = 2,
 };
 
+enum class EFetchState {
+    OK = 0,
+    NO_DATA = 1,
+    FINISHED = 2,
+};
+
 struct IRuntimeNode {
     virtual ~IRuntimeNode() = default;
     virtual bool Next(TRowSet& rowSet) = 0;
 
-    virtual bool Fetch(TRowSet& rowSet) = 0;
+    virtual EFetchState Fetch(TRowSet& rowSet) = 0;
     virtual EState Execute() = 0;
 
-    IRuntimeNode* OutputNode_ = nullptr;
-    IRuntimeNode* OutputNode() {
+    virtual bool IsFinished() const = 0;
+    virtual void Run(TRowSet& rowSet) = 0;
+
+    virtual IRuntimeNode* InputNode() = 0;
+    virtual IRuntimeNode* OutputNode() = 0;
+
+    virtual void PushOutput(TRowSet&& rowSet) = 0;
+    virtual void SetOutputNode(IRuntimeNode* node) = 0;
+};
+
+struct TNode : virtual IRuntimeNode {
+    IRuntimeNode* InputNode() override {
+        return nullptr;
+    }
+
+    IRuntimeNode* OutputNode() override {
+        return nullptr;
+    }
+
+    void PushOutput(TRowSet&& rowSet) override
+    { }
+
+    void SetOutputNode(IRuntimeNode* node)  override
+    { }
+};
+
+struct TNodeWithOutput : virtual TNode {
+    IRuntimeNode* OutputNode() override {
         return OutputNode_;
     }
 
-    virtual IRuntimeNode* InputNode() {
-        return nullptr;
+    void SetOutputNode(IRuntimeNode* node) override {
+        OutputNode_ = node;
+    }
+
+    EFetchState Fetch(TRowSet& rowSet) override {
+        if (!Output_.empty()) {
+            rowSet = std::move(Output_.front()); Output_.pop_front();
+            return EFetchState::OK;
+        }
+
+        return IsFinished() ? EFetchState::FINISHED : EFetchState::NO_DATA;
+    }
+
+    void PushOutput(TRowSet&& rowSet) override
+    {
+        Output_.emplace_back(std::move(rowSet));
+    }
+
+    bool Next(TRowSet& rowSet) override {
+        if (IsFinished()) {
+            return false;
+        }
+
+        Run(rowSet);
+        return true;
+    }
+
+    IRuntimeNode* OutputNode_ = nullptr;
+    std::deque<TRowSet> Output_;
+};
+
+struct TNodeWithInput : virtual TNode {
+    TNodeWithInput(IRuntimeNode* input)
+        : InputNode_(input)
+    { }
+
+    IRuntimeNode* InputNode() override {
+        return InputNode_;
+    }
+
+    EFetchState Fetch(TRowSet& rowSet) override {
+        return IsFinished() ? EFetchState::FINISHED : EFetchState::NO_DATA;
+    }
+
+    bool IsFinished() const override {
+        return InputConsumed_;
+    }
+
+    EState Execute() override {
+        TRowSet rowSet;
+        if (Input_.empty()) {
+            auto state = InputNode_->Fetch(rowSet);
+            if (state == EFetchState::OK) {
+                Input_.emplace_back(std::move(rowSet));
+            } else if (state == EFetchState::FINISHED) {
+                InputConsumed_ = true;
+            }
+        }
+
+        if (Input_.empty()) {
+            return InputConsumed_ ? EState::FINISHED : EState::NEED_DATA;
+        }
+
+        for (auto&& input : Input_) {
+            Run(input);
+            PushOutput(std::move(input));
+        }
+        Input_.clear();
+
+        return InputConsumed_ ? EState::FINISHED : EState::OK;
+    }
+
+    bool Next(TRowSet& rowSet) override {
+        if (!InputNode_->Next(rowSet)) {
+            return false;
+        }
+
+        Run(rowSet);
+
+        return true;
+    }
+
+    IRuntimeNode* InputNode_ = nullptr;
+    bool InputConsumed_ = false;
+    std::deque<TRowSet> Input_;
+};
+
+struct TNodeWithInputOutput : virtual TNodeWithInput, virtual TNodeWithOutput {
+    TNodeWithInputOutput(IRuntimeNode* input)
+    {
+        InputNode_ = input;
+    }
+
+    bool Next(TRowSet& rowSet) override {
+        return TNodeWithInput::Next(rowSet);
+    }
+
+    EFetchState Fetch(TRowSet& rowSet) override {
+        return TNodeWithOutput::Fetch(rowSet);
     }
 };
 
-struct TScan : IRuntimeNode {
-    bool NSets;
+struct TScan : virtual TNodeWithOutput {
+    int NSets;
     int Seed;
 
     int Ncols = 16;
 
     int SetId = 0;
 
-    std::deque<TRowSet> Output;
-
     TScan(int nsets, int seed)
         : NSets(nsets)
         , Seed(seed)
     { }
 
-    EState Execute() {
-        GenerateRowSet(Output.emplace_back(), Ncols, &Seed);
-        SetId ++;
-        return SetId == NSets ? EState::FINISHED : EState::OK;
+    bool IsFinished() const override {
+        return SetId == NSets;
     }
 
-    bool Fetch(TRowSet& rowSet) {
-        if (Output.empty() && SetId == NSets) {
-            return false;
-        }
-
-        if (!Output.empty()) {
-            rowSet = std::move(Output.front()); Output.pop_front();
-        }
-
-        return true;
-    }
-
-    bool Next(TRowSet& rowSet) {
-        if (SetId == NSets) {
-            return false;
-        }
-
+    void Run(TRowSet& rowSet) override {
         GenerateRowSet(rowSet, Ncols, &Seed);
         SetId ++;
+    }
 
-        return true;
+    EState Execute() override {
+        Run(Output_.emplace_back());
+        return IsFinished() ? EState::FINISHED : EState::OK;
     }
 };
 
-struct TFilter : IRuntimeNode {
+struct TFilter : virtual TNodeWithInputOutput {
     int Seed;
     int Percent;
-    IRuntimeNode* Input;
-
-    std::deque<TRowSet> InputRowSets;
-    std::deque<TRowSet> Output;
-    bool InputConsumed = false;
 
     TFilter(int seed, int percent, IRuntimeNode* input)
-        : Seed(seed)
+        : TNodeWithInput(input)
+        , TNodeWithInputOutput(input)
+        , Seed(seed)
         , Percent(percent)
-        , Input(input)
     {
-        Input->OutputNode_ = this;
+        InputNode_->SetOutputNode(this);
     }
 
-    IRuntimeNode* InputNode() {
-        return Input;
-    }
-
-    bool Fetch(TRowSet& rowSet) {
-        if (Output.empty() && InputConsumed) {
-            return false;
-        }
-
-        if (!Output.empty()) {
-            rowSet = std::move(Output.front()); Output.pop_front();
-        }
-
-        return true;
-    }
-
-    EState Execute() {
-        TRowSet rowSet;
-        if (InputRowSets.empty()) {
-            if (Input->Fetch(rowSet)) {
-                if (!rowSet.Empty()) {
-                    InputRowSets.emplace_back(std::move(rowSet));
-                }
-            } else {
-                InputConsumed = true;
-            }
-        }
-
-        if (InputRowSets.empty()) {
-            return InputConsumed ? EState::FINISHED : EState::NEED_DATA;
-        }
-
-        for (auto&& input : InputRowSets) {
-            Filter(input);
-            Output.emplace_back(std::move(input));
-        }
-        InputRowSets.clear();
-
-        return InputConsumed? EState::FINISHED : EState::OK;
-    }
-
-    void Filter(TRowSet& rowSet) {
+    void Run(TRowSet& rowSet) override {
         for (int i = 0; i < rowSet.Col.size(); ++i) {
             if (rowSet.Selection[i]) {
                 int k = (RandomInt(&Seed) % 100);
@@ -175,102 +243,39 @@ struct TFilter : IRuntimeNode {
             }
         }
     }
-
-    bool Next(TRowSet& rowSet) {
-        if (!Input->Next(rowSet)) {
-            return false;
-        }
-
-        Filter(rowSet);
-
-        return true;
-    }
 };
 
-struct TPrinter : IRuntimeNode {
-    IRuntimeNode* Input;
-
-    std::deque<TRowSet> InputRowSets;
-    std::deque<TRowSet> Output;
-    bool InputConsumed = false;
-
+struct TPrinter : virtual TNodeWithInput {
     TPrinter(IRuntimeNode* input)
-        : Input(input)
-    {
-        Input->OutputNode_ = this;
-    }
+        : TNodeWithInput(input)
+    { }
 
-    IRuntimeNode* InputNode() {
-        return Input;
-    }
-
-    bool Fetch(TRowSet& rowSet) {
-        if (Output.empty() && InputConsumed) {
-            return false;
-        }
-
-        if (!Output.empty()) {
-            rowSet = std::move(Output.front()); Output.pop_front();
-        }
-
-        return true;
-    }
-
-    EState Execute() {
-        TRowSet rowSet;
-        if (InputRowSets.empty()) {
-            if (Input->Fetch(rowSet)) {
-                if (!rowSet.Empty()) {
-                    InputRowSets.emplace_back(std::move(rowSet));
-                }
-            } else {
-                InputConsumed = true;
-            }
-        }
-
-        if (InputRowSets.empty()) {
-            return InputConsumed ? EState::FINISHED : EState::NEED_DATA;
-        }
-
-        for (auto&& input : InputRowSets) {
-            Print(input);
-        }
-        InputRowSets.clear();
-
-        return InputConsumed ? EState::FINISHED : EState::OK;
-    }
-
-    void Print(TRowSet& rowSet) {
+    void Run(TRowSet& rowSet) override {
         for (int i = 0; i < rowSet.Col.size(); ++i) {
             if (rowSet.Selection[i]) {
                 std::cout << rowSet.Col[i] << "\n";
             }
         }
     }
-
-    bool Next(TRowSet& rowSet) {
-        if (!Input->Next(rowSet)) {
-            return false;
-        }
-
-        Print(rowSet);
-
-        return true;
-    }
 };
 
 struct TScheduler {
     std::list<IRuntimeNode*> Ready;
+    std::unordered_set<IRuntimeNode*> Scheduled;
+
+    TScheduler() { }
 
     void Schedule(IRuntimeNode* node) {
-        if (node) {
+        if (node != nullptr && !Scheduled.contains(node)) {
             Ready.emplace_back(node);
+            Scheduled.emplace(node);
         }
     }
 
     void Run() {
         while (!Ready.empty()) {
             auto node = Ready.front(); Ready.pop_front();
+            Scheduled.erase(node);
             auto state = node->Execute();
             if (state == EState::NEED_DATA) {
                 Schedule(node->InputNode());
