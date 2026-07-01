145c529a7e8e6e4c48f1fe10baff8d7b1d72207f
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-07-01 08:16:54 +0100

    Add scheduler runtime node bridge

---
+++ qdb/scheduler/runtime_node.cpp
@@ -0,0 +1,71 @@
+#include <qdb/scheduler/runtime_node.h>
+
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+namespace NQdb {
+namespace NScheduler {
+
+TBufferedSchedulerOutput::~TBufferedSchedulerOutput()
+{
+    for (size_t i = Next_; i < RowSets_.size(); ++i) {
+        if (RowSets_[i].RefCount) {
+            Release(&RowSets_[i]);
+        }
+    }
+}
+
+void TBufferedSchedulerOutput::Add(const TRowSet& rowSet)
+{
+    auto copy = rowSet;
+    Retain(&copy);
+    RowSets_.push_back(copy);
+}
+
+bool TBufferedSchedulerOutput::Next(TRowSet& rowSet)
+{
+    if (Next_ >= RowSets_.size()) {
+        return false;
+    }
+    rowSet = RowSets_[Next_];
+    RowSets_[Next_] = {};
+    ++Next_;
+    return true;
+}
+
+std::shared_ptr<TSinkCode> MakeBufferedSchedulerSinkCode()
+{
+    return std::make_shared<TSinkCode>(
+        [](void* state, const TRowSet& rowSet) {
+            auto* output = static_cast<TBufferedSchedulerOutput*>(state);
+            output->Add(rowSet);
+        });
+}
+
+TRuntimeSchedulerPipeline::TRuntimeSchedulerPipeline(
+    std::unique_ptr<TTaskGraph> graph,
+    TSettings settings,
+    NQumir::NAst::TTypePtr outputType,
+    std::shared_ptr<TBufferedSchedulerOutput> output)
+    : Graph_(std::move(graph))
+    , Settings_(settings)
+    , OutputType_(std::move(outputType))
+    , Output_(std::move(output))
+{}
+
+bool TRuntimeSchedulerPipeline::Next(TRowSet& rowSet)
+{
+    if (!Started_) {
+        Started_ = true;
+        std::string error;
+        TSchedulerExecutor executor(*Graph_, Settings_);
+        if (!executor.Run(&error)) {
+            throw std::runtime_error(error);
+        }
+    }
+    return Output_->Next(rowSet);
+}
+
+} // namespace NScheduler
+} // namespace NQdb
