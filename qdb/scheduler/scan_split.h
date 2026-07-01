ca3a3cff151ed7888a4a44b832389df6d34b6a6f
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-07-01 08:08:42 +0100

    Add scheduler scan split planning

---
+++ qdb/scheduler/scan_split.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <qdb/scheduler/settings.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+namespace NQdb {
+namespace NScheduler {
+
+struct TScanRowGroup {
+    size_t RowGroup = 0;
+    int64_t RowCount = 0;
+    int64_t ByteSize = 0;
+};
+
+struct TScanSplit {
+    size_t FirstRowGroup = 0;
+    size_t RowGroupCount = 0;
+    int64_t RowCount = 0;
+    int64_t ByteSize = 0;
+    bool SerialRead = false;
+};
+
+std::vector<TScanSplit> BuildScanSplits(
+    const std::vector<TScanRowGroup>& rowGroups,
+    const TScanSplitSettings& settings);
+
+} // namespace NScheduler
+} // namespace NQdb
