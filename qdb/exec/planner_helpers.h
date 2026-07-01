b4ccf0c6b93a2e55afe3dfb9a87943e4aa876ba0
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-07-01 23:50:53 +0100

    Move scheduler plan lowering out of planner

---
+++ qdb/exec/planner_helpers.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <qdb/exec/unary_stream_exec.h>
+#include <qdb/exec/sort_exec.h>
+#include <qdb/plan/ops/filter.h>
+#include <qdb/plan/ops/project.h>
+#include <qdb/plan/ops/source.h>
+#include <qdb/plan/ops/sort.h>
+
+#include <iosfwd>
+#include <string_view>
+#include <vector>
+
+namespace NQdb {
+
+struct TUnaryRuntimeProcess {
+    TRuntimeUnaryStreamingKernel::TProcess Process;
+    NQumir::NAst::TTypePtr OutputType;
+};
+
+struct TSortRuntimeProcess {
+    std::vector<TSortColumnRef> KeyColumns;
+    TSortRadixKernel RadixKernel;
+};
+
+NQumir::NAst::TTypePtr BuildSourceRuntimeType(TSourceOperator& src);
+
+TUnaryRuntimeProcess BuildFilterRuntimeProcess(
+    TFilterOperator& filter,
+    const NQumir::NAst::TTypePtr& inputType,
+    std::ostream* diagnostics);
+
+TUnaryRuntimeProcess BuildProjectRuntimeProcess(
+    TProjectOperator& project,
+    const NQumir::NAst::TTypePtr& inputType,
+    std::ostream* diagnostics);
+
+TSortRuntimeProcess BuildSortRuntimeProcess(
+    const NQumir::NAst::TStructType& inputType,
+    const std::vector<TSortKey>& keys,
+    std::string_view kernelName,
+    std::ostream* diagnostics);
+
+} // namespace NQdb
